dd8bc82b6dd532ca0c642b8795e7a4a0a921f890
Ohtaeyong <kw7946@naver.com>
2023-10-24 20:50:03 +0900

    day01

---
+++ day01/src/exam01/Simple.java
@@ -0,0 +1,14 @@
+package exam01;
+
+public class Simple {
+
+    /* 속성 */
+    private String message;
+
+    /* 생성자
+    public Sample(String message) {
+        this.message = message;
+    }
+
+     */
+}
